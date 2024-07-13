dead93e2fa594bd14f6a51a87d11ebe531038166
James Rutter <jamesdavidrutter@gmail.com>
2024-07-13 04:48:07 -0400

    feat: adds post route to log sensor data

--- main.ts
+++ main.ts
@@ -12,5 +12,35 @@ app.get('/api', (c) => {
   });
 });
 
+app.post('/api/:sensor', async (c) => {
+  console.log('POST /api/:sensor');
+
+  const sensor = c.req.param('sensor');
+  console.log(`\t Sensor: ${sensor}`);
+
+  // Get the body and the value from the sensor reading
+  const { value } = await c.req.json();
+  const temperature = parseFloat(value);
+
+  return c.json({
+    ok: true,
+    message: 'Received sensor reading and saved to the database.',
+    sensor,
+    temperature,
+  });
+});
+
+app.get('/api/:sensor', (c) => {
+  console.log('GET /api/:sensor');
+  const sensor = c.req.param('sensor');
+  console.log(`\t Sensor: ${sensor}`);
+
+  return c.json({
+    ok: true,
+    message: 'Arduino Web API',
+    sensor,
+  });
+});
+
 // Start the server
 Deno.serve(app.fetch);
